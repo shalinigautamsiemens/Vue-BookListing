83a245f1d698375a81d6e40c1a889456545673d8
Shalini Gautam <shalini.gautam@siemens.com>
2019-06-02 17:26:34 +0530

    Loop through a list of books
    Loop through a list of books

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <h1>{{ title }}</h1>
+        <ul>
+            <li v-for="book in books">{{book.title}}:{{book.author}}</li>
+        </ul>
     </div>
 </template>
 
@@ -9,7 +12,12 @@ export default {
     name:'BookList',
     data() {
         return {
-            title: 'All Books'
+            title: 'All Books',
+            books: [
+                {title:'Self-Reliance',author:'Ralph Waldo Emerson'},
+                {title:'American Gods', author:'Neil Galman'},
+                {title:'Amusing Ourlselves to Death', author:'Neil Postman'}
+            ]
         }
 
     }
@@ -18,7 +26,15 @@ export default {
 
 <style>
 h1,h2 {
-  font-weight: normal  
-}    
+    font-weight: normal  
+}   
+ul {
+    list-style-type: none;
+    padding: 0;
+} 
+li {
+    display: block;
+    margin: 0 10px;
+}
 </style>
 
