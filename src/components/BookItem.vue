b5d8629786a54f88e90a6031938eb63863a95dc0
Shalini Gautam <shalini.gautam@siemens.com>
2019-06-03 08:52:51 +0530

    Created Child Component

---
+++ src/components/BookItem.vue
@@ -0,0 +1,23 @@
+<template>
+    <li>{{book.title}}:{{book.author}}</li>
+</template>
+
+<script>
+export default {
+    name: 'BookItem',
+    props: ['book'],
+    data() {
+        return {
+            
+        }
+    }
+
+}
+</script>
+
+<style>
+    li {
+    display: block;
+    margin: 0 10px;
+}
+</style>
